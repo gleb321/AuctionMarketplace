b62bb54535748d041bc576b1cb99df4790ffc6cc
Gleb Evlakhov <42343481+gleb321@users.noreply.github.com>
2022-08-29 22:36:05 +0300

    upd

--- AuctionLiveService/Controllers/AuctionLiveController.cs
+++ AuctionLiveService/Controllers/AuctionLiveController.cs
@@ -1,28 +1,41 @@
 using System;
-using System.Globalization;
-using System.Threading;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
-using System.Timers;
 using Microsoft.AspNetCore.Mvc;
 using AuctionLiveService.Models;
 using AuctionLiveService.Services;
+using AuctionMarketplaceLibrary;
 
 namespace AuctionLiveService.Controllers {
     [ApiController]
     [Route("/auction_live/")]
     public class AuctionLiveController: Controller {
+        private readonly HttpClient _client;
         private readonly AuctionAlertService _auctionAlertService;
         private readonly AuctionManagementService _managementService;
 
-        public AuctionLiveController(AuctionManagementService managementService,  AuctionAlertService alertService) {
+        public AuctionLiveController(AuctionManagementService managementService,  AuctionAlertService alertService, HttpClient client) {
             _managementService = managementService;
             _auctionAlertService = alertService;
+            _client = client;
         }
         
         [HttpPost("add")]
-        public IActionResult Add([FromBody] Auction auction) {
+        public async Task<IActionResult> Add([FromBody] Auction auction) {
             _managementService.Add(auction.Id, auction.StartTime, true, _auctionAlertService);
             _managementService.Add(auction.Id, auction.FinishTime, false, _auctionAlertService);
+            
+            //TODO Подумать об использовании разных http клиентов
+            var data = new {auction.Id, auction.SellerId, CurrentBid = auction.StartPrice};
+            var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+            var url = $"http://{Config.BidServiceHost}:{Config.BidServiceHost}/add";
+            var response = await _client.PostAsync(new Uri(url), content);
+            if (response.StatusCode != HttpStatusCode.OK) {
+                return BadRequest("Trouble adding auction to Bid service.");
+            }
             
             return Ok("Auction was successfully added.");
         }
--- BidService/Models/Auction.cs
+++ BidService/Models/Auction.cs
@@ -7,7 +7,7 @@ namespace BidService.Models {
         public int Id { get; init; }
         [Required]
         [EmailAddress]
-        public string? Owner { get; init; }
+        public string? SellerId { get; init; }
         public bool IsActive { get; set; }
         [Range(1, 1000000)]
         public decimal CurrentBid { get; set; }
